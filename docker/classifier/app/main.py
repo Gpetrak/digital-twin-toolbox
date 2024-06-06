b4f4108c904ddc843d12f07d98ad6859c0f1d232
gpetrak <gkpetrak@gmail.com>
2024-06-06 10:38:25 +0300

    adding OpenPointClass command to the main.py

--- docker/classifier/app/main.py
+++ docker/classifier/app/main.py
@@ -5,21 +5,19 @@ import subprocess
 
 app = FastAPI()
 
-'''
-@app.get("/")
-def read_root():
-    return {"Hello": "Big world"}
 
+def first_command(input_file: str, output_file: str, model: str, color: str):
+    
+    # Define and run the command
+    myprocess = subprocess.run(["../build/pcclassify", input_file, output_file, model, color], stdout=subprocess.PIPE)
+    output = myprocess.stdout.decode('utf-8')
+    return output
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-'''
-
-def first_command(cmd: str):
-    return subprocess.run(cmd)
-
-@app.get("/test/{cmd}")
-async def test_command(cmd: str, background_tasks: BackgroundTasks):
-    background_tasks.add_task(first_command, cmd)
-    return {"message": "test command sent in the background"}
+@app.post("/test/classifier/{query_id}")
+async def test_command(query_id: int, background_tasks: BackgroundTasks):
+    input_file = '/data/dataset-path/ML_models/test.laz'
+    output_file = '/data/dataset-path/ML_models/test_class.laz'
+    model = '/data/dataset-path/ML_models/modelV13_mono.bin'
+    color = '--color'
+    background_tasks.add_task(query_id, first_command, input_file, output_file, model, color)
+    return {"message": "Your command is running with the ID: {}... Please wait for the response".format(query_id)}
